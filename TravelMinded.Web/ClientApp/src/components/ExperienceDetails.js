f74d4d1186c4f71f3960fe0a52386a0cbba0140b
MK <searedahi@outlook.com>
2018-11-28 09:36:19 -0800

    'CommitedFromBATTMWeb'

--- TravelMinded.Web/ClientApp/src/components/ExperienceDetails.js
+++ TravelMinded.Web/ClientApp/src/components/ExperienceDetails.js
@@ -4,9 +4,8 @@ import { connect } from 'react-redux';
 import { experienceDetailsActionCreators } from '../store/ExperienceDetails';
 import { Col, Row, Button, Carousel } from 'react-bootstrap';
 import './ExperienceDetails.css';
-
-
-
+import NumericInput from 'react-numeric-input';
+import Calendar from 'react-calendar';
 
 class ExperienceDetails extends Component {
 
@@ -19,32 +18,69 @@ class ExperienceDetails extends Component {
 
     render() {
         return (
-            renderExperienceDetails(this.props.experienceDetails)
+            <section>
+                {renderSpotlightCarousel(this.props.experienceDetails)}
+                {renderExperienceInfoAndAvail(this.props.experienceDetails)}    
+            </section>
         );
     }
 }
 
 
 
-function renderExperienceDetails(experienceDetails) {
+function renderExperienceInfoAndAvail(experienceDetails) {
+
+
+    var todaysDate = new Date(2019, 0, 1);
+    if (experienceDetails.nextAvailableDate !== undefined) {
+        var serverDate = new Date(experienceDetails.nextAvailableDate);
+        todaysDate = serverDate;
+    }
     return (
-        <div>
-            {renderSpotlightCarousel(experienceDetails)}
-            <Row>
-                <Col sm={12} md={8} lg={8} >
-                    <div className="experienceDetailsSingleLine">
-                        <span className="experienceDetailsName">{experienceDetails.name}</span>{renderCompanyName(experienceDetails)}
-                    </div>
-                    <div>
-                        <span className="experienceDetailsDurationLocation">{experienceDetails.durationFormatted}  {experienceDetails.location}</span>
-                    </div>
-                    <p className="experienceDetailsDescription">{experienceDetails.description}</p>
-                    <p>{experienceDetails.nextAvailableDateFormatted}</p>
-                </Col>
-            </Row>
-        </div>
+        <Row>
+            <Col sm={12} md={8} lg={8}>
+                <div className="experienceDetailsSingleLine">
+                    <span className="experienceDetailsName">{experienceDetails.name}</span>{renderCompanyName(experienceDetails)}
+                </div>
+                <div>
+                    <span className="experienceDetailsDurationLocation">{experienceDetails.durationFormatted}  {experienceDetails.location}</span>
+                </div>
+                <p className="experienceDetailsDescription">{experienceDetails.description}</p>
+                <p>{experienceDetails.nextAvailableDateFormatted}</p>
+            </Col>
+            <Col sm={12} md={4} lg={4}>
+                <h4>Discouinted rate</h4>
+                <h2>{experienceDetails.currentAdultPrice}</h2>
+                <p>Check Availability:</p>
+                <NumericInput
+                    id="headcount"
+                    className="form-control"
+                    min={1}
+                    max={10}
+                    onChange={onHeadcountChange}
+                />
+                <section id="experienceDetailsCalendarSection">
+                    <Calendar
+                        onChange={onExperienceDateChange}
+                        value={todaysDate}
+                    />
+                </section>
+            </Col>
+        </Row>
     );
 }
+
+function onHeadcountChange(value, event) {
+    console.log('Headcount Changed: ' + value);
+}
+
+
+function onExperienceDateChange(dateSelected, event) {
+    console.log('Date Selected: ' + dateSelected);
+}
+
+
+
 
 
 function renderSpotlightCarousel(experienceDetails) {
